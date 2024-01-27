b6c81ebaab974a61dfc88d02e567c687f8be8a41
mac <mac@macui-MacBookAir.local>
2024-01-28 01:32:01 +0900

    레시피 페이지 헤드메뉴바 작업

--- src/page/Recipe.jsx
+++ src/page/Recipe.jsx
@@ -1,11 +1,9 @@
-import Menubutton from "../components/Menubutton";
+import Headerbar from "../components/Headerbar";
 
 function Recipe() {
     return ( 
         <>
-        <Menubutton name="뒤로가기"/>
-        <Menubutton name="내 레시피"/>
-        <Menubutton name="레시피 만들기"/>
+        <Headerbar />
         </>
      );
 }
